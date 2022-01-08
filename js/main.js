d275944475f321dc486e4f029a252a4dd5a4868d
unknown <nijat.bakhtiyarli@iatc.edu.az>
2022-01-08 10:50:49 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var elements = document.getElementsByClassName("box");
+for(var i = 0; i < elements.length; i++){
+    elements[i].onclick = function(){
+        
+        var el = elements[0];
+        while(el){
+            if(el.tagName === "li"){
+                el.classList.remove("active");
+            }
+            el = el.nextSibling;
+        }
+
+        this.classList.add("active");
+    }
+}
+
+
